96a9c00ef2d5b1833d27af1b735feb7379980ed0
taku1010 <takumi.guitar1010@gmail.com>
2018-08-11 00:20:36 +0900

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by 西村拓海 on 2018/08/11.
+//  Copyright © 2018年 西村拓海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
